1652df7e0976c8f9630bfcf779eaf159901eed80
Daniel G. Torres <danielguevara1500@gmail.com>
2025-05-28 20:36:23 -0500

    commit

--- proyecto/operaciones.py
+++ proyecto/operaciones.py
@@ -1,16 +1,16 @@
 from productos import obtener_producto
 
 def calcular_total(lista_productos):
-    """ Calcular el total de la compra. """
+    """ Calcular el total de la compra """
     total = 0
     for producto in lista_productos:
         total += producto['precio']
-        return calcular_total
-
+        return total
+    
 def agregar_producto(id_producto, cantidad, lista_productos):
-    """ Agregar un producto a la lista de compra y registrar la venta"""
+    """ Agregar un producto a la lista de compra """
     producto = obtener_producto(id_producto)
     if producto:
-        lista_productos.append({"nombre": producto["nombre"], "precio": producto["precio"]* cantidad})
+        lista_productos.append({"nombre": producto ["nombre"], "precio": producto ["precio"] * cantidad})
     else:
-        raise ValueError(f"Producto con ID {id_producto} no encontrado.")
+        raise ValueError(f"Producto con ID {id_producto} no encotrado ")
